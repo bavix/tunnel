a76ec2e9fe71f0a7c6e3c3b64bc8e66ba197c53d
Babichev Maxim <maksim.babichev95@gmail.com>
2020-02-02 11:15:03 +0300

    fix port autossh service

--- app/Services/TunnelService.php
+++ app/Services/TunnelService.php
@@ -14,7 +14,7 @@ class TunnelService
     {
         $local = escapeshellcmd("{$tunnel->where_to}:localhost:{$tunnel->where_from}");
         $address = escapeshellcmd($tunnel->address);
-        @exec('autossh -M 20000 -f -N ' . $address . ' -R ' . $local . ' -C', $output);
+        @exec('autossh -M ' . (20000 + $tunnel->getKey()) . ' -f -N ' . $address . ' -R ' . $local . ' -C', $output);
     }
 
 }
